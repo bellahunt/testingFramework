5ef771c14ec3609167a2b58128c9e7aa919c8732
bellahunt <bellah292@gmail.com>
2018-08-29 10:54:52 +1000

    ok

--- app/src/test/java/au/edu/unimelb/comp30022/controllertesting/ControllerTest.java
+++ app/src/test/java/au/edu/unimelb/comp30022/controllertesting/ControllerTest.java
@@ -3,16 +3,29 @@ package au.edu.unimelb.comp30022.controllertesting;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.junit.MockitoJUnitRunner;
+import static org.mockito.Mockito.*;
+import org.junit.runner.RunWith;
+import android.location.Location;
 
 import static org.junit.Assert.*;
 
 /**
  * Created by william on 31/8/17.
  */
+@RunWith(MockitoJUnitRunner.class)
 public class ControllerTest {
+
+    private AddressTools addressToolsMock;
+    private PostcodeValidator postcodeValidatorMock;
+    private PostageRateCalculator postageRateCalculatorMock;
+
     @Before
     public void setUp() throws Exception {
-
+        // create other mocks
+        addressToolsMock = mock(AddressTools.class);
+        postcodeValidatorMock = mock(PostcodeValidator.class);
+        postageRateCalculatorMock = mock(PostageRateCalculator.class);
     }
 
     @After
@@ -23,6 +36,38 @@ public class ControllerTest {
     @Test
     public void testPostageCost() throws Exception {
 
-        assertTrue(true);
+    }
+
+    @Test
+    public void calculateButtonPressed() {
+        // pass mock objects as dependency to constructor
+        Controller controller = new Controller(addressToolsMock, postcodeValidatorMock, postageRateCalculatorMock);
+
+        // configure mock expected interaction
+        when(postageRateCalculatorMock.computeCost(any(Location.class), any(Location.class))).thenReturn(5);
+        when(postcodeValidatorMock.isValid(any(String.class))).thenReturn(true);
+        when(addressToolsMock.locationFromAddress(any(Address.class))).thenReturn(new Location("3000"));
+
+        // call method being tested
+        EditText sourcePostCodeText = new EditText();
+        sourcePostCodeText.setText("Not null");
+        controller.sourcePostCodeField = sourcePostCodeText;
+
+        EditText destPostCodeText = new EditText();
+        destPostCodeText.setText("Not null");
+        controller.destinationPostCodeField = destPostCodeText;
+
+        TextView costLabel = new TextView();
+        costLabel.setText("Not null");
+        controller.costLabel = costLabel;
+
+        controller.calculateButtonPressed();
+
+        // check if mock had expected interaction
+        // computeCost method called in calculateButtonPressed, tests if directed correctly / does the right thing
+        verify(postageRateCalculatorMock).computeCost(any(Location.class), any(Location.class));
+
+        // assert results
+        assertEquals("$5", controller.costLabel.getText());
     }
 }
